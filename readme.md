1c70a77db783e7533672ac049bb5550f562d518d
Kamil Wazydrag <kamil_wazydrag@epam.com>
2019-01-31 11:00:59 +0100

    dodanie przykladu, wraz z krotkim opisem jak uruchomic, kazdy klient jest uruchamiany jako nowy watek

---
+++ readme.md
@@ -0,0 +1,2 @@
+klienta odpalamy w terminalu uzywajac telnet, port 8189
+przyklad : telnet localhost 8189
--- src/main/java/com/gang/oskara/battle_net_ship/App.java
+++ src/main/java/com/gang/oskara/battle_net_ship/App.java
@@ -1,7 +1,82 @@
 package com.gang.oskara.battle_net_ship;
 
-class App {
-    public static void main(String[] args) {
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * Program implementujący wielowątkowy serwer nasłuchujący na porcie 8189
+ * i wysyłający echo informacji otrzymanej od klientów.
+ * @author Cay Horstmann
+ * @version 1.22 2016-04-27
+ */
+public class App
+{
+    public static void main(String[] args )
+    {
+        try (ServerSocket s = new ServerSocket(8189))
+        {
+            int i = 1;
+
+            while (true)
+            {
+                Socket incoming = s.accept();
+                System.out.println("Spawning " + i);
+                Runnable r = new ThreadedEchoHandler(incoming);
+                Thread t = new Thread(r);
+                t.start();
+                i++;
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * Klasa obsługująca komunikację z pojedynczym klientem.
+ */
+class ThreadedEchoHandler implements Runnable
+{
+    private Socket incoming;
+
+    /**
+     * Tworzy obiekt obsługi.
+     * @param i gniazdo wejściowe
+     */
+    public ThreadedEchoHandler(Socket incomingSocket)
+    {
+        incoming = incomingSocket;
+    }
+
+    public void run()
+    {
+        try (InputStream inStream = incoming.getInputStream();
+             OutputStream outStream = incoming.getOutputStream())
+        {
+            Scanner in = new Scanner(inStream, "UTF-8");
+            PrintWriter out = new PrintWriter(
+                    new OutputStreamWriter(outStream, "UTF-8"),
+                    true /* autoFlush */);
+
+            out.println("Witaj! Wpisz BYE, by zakończyć.");
+
+            // wyświetla echo
+            boolean done = false;
+            while (!done && in.hasNextLine())
+            {
+                String line = in.nextLine();
+                out.println("Echo: " + line);
+                if (line.trim().equals("BYE"))
+                    done = true;
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
